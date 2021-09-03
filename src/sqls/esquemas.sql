bcb2f1b88ca18df15462d71b33a72f372440529c
Felipe Morales <felipe.morales.querol@gmail.com>
2021-09-02 22:22:46 -0300

    Update esquemas.sql

--- src/sqls/esquemas.sql
+++ src/sqls/esquemas.sql
@@ -4,32 +4,35 @@ create table bandas (
  integrantes integer not null,
  fecha_inicio date not null,
  fecha_separación date,
- pais varchar(100) not null,
+ pais_id integer not null,
  primary key (id)
 );
 
+drop table if exists canciones;
 
 create table canciones (
  id integer not null auto_increment,
  nombre varchar(100) not null,
  duracion integer not null,
- album integer not null,
- banda integer not null,
+ album_id integer not null, 
+ banda_id integer not null, 
  fecha_publicación date not null,
  primary key (id)
 );
 
-ALTER TABLE canciones ADD CONSTRAINT canciones_FK FOREIGN KEY (banda) REFERENCES bandas(id);
 
+drop table if exists albumes;
 
 create table albumes (
  id integer not null auto_increment,
  nombre varchar(100) not null,
- banda integer not null,
+ banda_id integer not null, /*--bandas*/
  fecha_publicación date not null,
  primary key (id)
 );
 
-ALTER TABLE albumes ADD CONSTRAINT albumes_FK FOREIGN KEY (banda) REFERENCES bandas(id);
 
-
+ALTER TABLE bandas ADD CONSTRAINT bandas_pais_FK FOREIGN KEY (pais_id) REFERENCES paises(id);
+ALTER TABLE canciones ADD CONSTRAINT canciones_FK FOREIGN KEY (banda_id) REFERENCES bandas(id);
+ALTER TABLE canciones ADD CONSTRAINT canciones_alb_FK FOREIGN KEY (album_id) REFERENCES albumes(id);
+ALTER TABLE albumes ADD CONSTRAINT albumes_FK FOREIGN KEY (banda_id) REFERENCES bandas(id);
